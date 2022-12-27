28add9cbec4236133313e3ec6197fe0ef593f31d
Bruno Santos <devbrunosantos@gmail.com>
2022-12-27 09:13:48 -0300

    fix: deploy error with cycles undefined

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -52,6 +52,8 @@ export function CyclesContextProvider({
       if (storedStateAsJSON) {
         return JSON.parse(storedStateAsJSON);
       }
+
+      return { cycles: [], activeCycleId: null };
     }
   );
 
